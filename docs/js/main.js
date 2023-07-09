d119e226c787053a8f86198eaeb81572dabfe6ab
Polilopus <126248195+Polilopus@users.noreply.github.com>
2023-07-09 14:05:24 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -13,4 +13,39 @@ $(function () {
   $('.blog-button__filter').removeClass('blog-button__filter--active')
   $(this).addClass('blog-button__filter--active')
   })
+
+
+  $('.customers-slider').slick ({
+    arrows: false,
+    dots: true,
+    appendDots: $('.customers-dots'),
+    waitForAnimate: false,
+
+  })
+
+  $('.customers-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.customers-slider').slick('slickPrev')
+  })
+  $('.customers-next').on('click', function (e) {
+    e.preventDefault()
+    $('.customers-slider').slick('slickNext')
+  })
+
+
+  $('.questions-acc__link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('questions-acc__link--active')) {
+      $(this).removeClass('questions-acc__link--active')
+      $(this).children('.questions-acc__text').slideUp()
+    } else {
+      $('.questions-acc__link').removeClass('questions-acc__link--active')
+      $('.questions-acc__text').slideUp()
+      $(this).addClass('questions-acc__link--active')
+      $(this).children('.questions-acc__text').slideDown()
+    }
+  })
+
+
  })
+
